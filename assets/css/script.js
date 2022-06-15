c36763eea4b7418da0a08c9a45ade2b1265dad8e
Adrian Firmalan <afirmalan@yahoo.com>
2022-06-15 14:52:00 -0400

    added ids and selectors

---
+++ assets/css/script.js
@@ -0,0 +1,8 @@
+// define html static selector
+var cityEl= document.querySelector("#city")
+var cityFormEl= document.querySelector("#city-form")
+
+// definition of functions
+
+
+// addevent listener
